aa951f648a8b63978b3bdc4b5ea73c58b0e1e4bc
Christian Müller <christian@kitsunet.de>
2014-09-12 16:25:12 +0200

    [TASK] Adapt to merged EelUtility in TYPO3.EEL
    This removes the EelUtility class from this package as the
    generic EelUtility was merged in TYPO3.EEL and can be used
    from there now.

--- Classes/TYPO3/TYPO3CR/Search/Indexer/AbstractNodeIndexer.php
+++ Classes/TYPO3/TYPO3CR/Search/Indexer/AbstractNodeIndexer.php
@@ -12,7 +12,7 @@ namespace TYPO3\TYPO3CR\Search\Indexer;
  *                                                                              */
 
 use TYPO3\TYPO3CR\Domain\Model\Node;
-use TYPO3\TYPO3CR\Search\Eel\EelUtility;
+use TYPO3\Eel\Utility as EelUtility;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\TYPO3CR\Search\Exception\IndexingException;
 
