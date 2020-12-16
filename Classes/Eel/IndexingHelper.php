2e4e1e43359d55a50f29d4335d1f3aba28696fcd
lorenzulrich <lorenz.ulrich@visol.ch>
2020-12-16 16:01:11 +0100

    TASK: Replace curly braces syntax for string offset access

--- Classes/Eel/IndexingHelper.php
+++ Classes/Eel/IndexingHelper.php
@@ -50,7 +50,7 @@ class IndexingHelper implements ProtectedContextAwareInterface
 
         $currentPath = '';
         $pathPrefixes = [];
-        if ($path{0} === '/') {
+        if (strpos($path, '/') === 0) {
             $currentPath = '/';
             $pathPrefixes[] = $currentPath;
         }
